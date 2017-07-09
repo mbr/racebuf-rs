7be8ac9fced8f581ccae03a42399f723a62b71fc
Marc Brinkmann <git@marcbrinkmann.de>
2017-07-09 17:17:12 +0200

    Added docs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 //! A racy buffer
 //!
 //! Implements a `RaceBuf`, which is a buffer that can be read and written
-//! to by multiple threads without checking consistency *at all*.
+//! to by multiple threads without checking consistency *at all*. It uses
+//! volatile read/write operations to make it thread "safe".
 //!
 //! Your program will be safe from crashing, but not from partial reads or
 //! interleaved writes.
@@ -11,12 +12,19 @@
 //! not an issue. Types that can be read in a single instruction, like small
 //! integers, will likely not suffer from these issues on some platforms.
 
+
 use std::ptr;
 
+/// Racy buffer
+///
+/// Guaranteed not to crash due to memory safety violations on get/set.
+///
+/// Guarantees almost nothing else, especially that values stored or loaded
+/// are actually valid values for type `T
 pub struct RaceBuf<T>(Vec<T>);
 
-
 impl<T: Clone + Copy> RaceBuf<T> {
+    /// Create a new buffer, initialized with `value`.
     #[inline]
     pub fn new_with_value(size: usize, value: T) -> RaceBuf<T> {
         let mut v = Vec::with_capacity(size);
@@ -24,6 +32,9 @@ impl<T: Clone + Copy> RaceBuf<T> {
         RaceBuf(v)
     }
 
+    /// Retrieve value stored at index
+    ///
+    /// Will return `None` if the index is out-of-bounds.
     #[inline(always)]
     pub fn get(&self, idx: usize) -> Option<T> {
         if idx >= self.0.len() {
@@ -33,12 +44,16 @@ impl<T: Clone + Copy> RaceBuf<T> {
         }
     }
 
+    /// Retrieve value stored at index without bounds checking
     #[inline(always)]
     pub unsafe fn get_unchecked(&self, idx: usize) -> T {
         // unsafe: bounds are not checked, pointer deref
         ptr::read_volatile(self.0.as_ptr().offset(idx as isize))
     }
 
+    /// Set value at index
+    ///
+    /// If `idx` is out-of-bounds, `set` will have no effect.
     #[inline(always)]
     pub fn set(&self, idx: usize, value: T) {
         if idx >= self.0.len() {
@@ -48,6 +63,10 @@ impl<T: Clone + Copy> RaceBuf<T> {
         }
     }
 
+    /// Set value at index without bounds checking
+    ///
+    /// Will definately cause undefined behaviour if `idx` is not within
+    /// bounds.
     #[inline(always)]
     pub unsafe fn set_unchecked(&self, idx: usize, value: T) {
         // unsafe: bounds are not checked, pointer deref, *const T to *mut T
@@ -56,14 +75,9 @@ impl<T: Clone + Copy> RaceBuf<T> {
 }
 
 impl<T: Clone + Copy + Default> RaceBuf<T> {
+    /// Create new buffer, initialized with default value
     #[inline]
     pub fn new(size: usize) -> RaceBuf<T> {
         RaceBuf::new_with_value(size, T::default())
     }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {}
 }
